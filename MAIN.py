d5de5c55c43f0186e72589301d9af77668471693
dwoloszin <3h2so4@gmail.comgit>
2023-04-19 14:44:00 -0300

    first Commit

--- MAIN.py
+++ MAIN.py
@@ -5,4 +5,7 @@ from selenium.webdriver.chrome.service import Service
 servico = Service(ChromeDriverManager().install())
 Navegador = webdriver.Chrome(service=servico)
 
+#http://10.98.4.40:8080/MicroStrategy/servlet/mstrWeb
+
+
 
